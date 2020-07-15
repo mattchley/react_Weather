513462ce357b5bd11a7aa4324a3f71b04daa6995
mattchley <mattchley@gmail.com>
2020-07-15 12:16:59 -0700

    proper input search

--- src/Pages/weather.js
+++ src/Pages/weather.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import API from '../Utils/API';
@@ -6,18 +6,37 @@ import API from '../Utils/API';
 
 
 function Weather() {
-    useEffect(() => {
-        API.getWeather('San Diego')
-        .then(res =>{
-            console.log(res.data)
-        })
-    }, [])
+
+    const [search, setSearch] = useState();
+    const [weather, setWeather] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        citySearch(search)
+    }
+
+    const citySearch = search =>{
+        API.getWeather(search)
+            .then(res => {
+                console.log(res.data)
+                setWeather(res.data.name)
+            })
+    }
 
     return (
         <Container>
             <h1>What's the weather?</h1>
             <Grid container spacing={3} justify={"space-evenly"}>
-                <p>here</p>
+                <Grid item md={3} xs={12}>
+                    <h1>Search here</h1>
+                    <input id="citySearch" onChange={(event, value) => setSearch(document.getElementById("citySearch").value)}></input>
+                    <button onClick={handleSubmit}>Search Cities</button>
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <h1>Results</h1>
+                    <p>{weather}</p>
+                </Grid>
             </Grid>
 
         </Container>
