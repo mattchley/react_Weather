5e857d2dd865f3ea34c998ef56c3fccebfc2ea3e
mattchley <mattchley@gmail.com>
2020-07-15 17:57:05 -0700

    search results render to page basic layout

--- src/Pages/weather.js
+++ src/Pages/weather.js
@@ -8,18 +8,41 @@ import API from '../Utils/API';
 function Weather() {
 
     const [search, setSearch] = useState();
-    const [weather, setWeather] = useState();
+    const [weather, setWeather] = useState([]);
 
     const handleSubmit = e => {
         e.preventDefault();
         citySearch(search)
     }
 
-    const citySearch = search =>{
+    const citySearch = search => {
         API.getWeather(search)
             .then(res => {
                 console.log(res.data)
-                setWeather(res.data.name)
+                let name = res.data.name;
+                let temp = res.data.main.temp
+                let humidity = res.data.main.humidity
+                let wind = res.data.wind.speed;
+                let icon = res.data.weather[0].icon;
+                let iconURL = `https://openweathermap.org/img/w/${icon}png`
+
+                setWeather([
+                    ...weather,
+                    {
+                        
+                        name: name,
+                        temp: temp,
+                        humidity: humidity,
+                        wind: wind,
+                        iconURL: iconURL
+                    }
+                ])
+
+                //  //    UV is a seperate call, Variables here
+                //  let lat = res.coord.lat;
+                //  let lon = res.coord.lon;
+                //  let uvURL = `https://api.openweathermap.org/data/2.5/uvi?appid=3a92bc0a0ca29575a3569a00c0268022&lat=${lat}&lon=${lon}`;
+                // setWeather(res.data)
             })
     }
 
@@ -29,13 +52,24 @@ function Weather() {
             <Grid container spacing={3} justify={"space-evenly"}>
                 <Grid item md={3} xs={12}>
                     <h1>Search here</h1>
-                    <input id="citySearch" onChange={(event, value) => setSearch(document.getElementById("citySearch").value)}></input>
+                    <input id="citySearch"
+                        onChange={(event, value) => setSearch(document.getElementById("citySearch").value)} />
                     <button onClick={handleSubmit}>Search Cities</button>
                 </Grid>
 
                 <Grid item md={6} xs={12}>
                     <h1>Results</h1>
-                    <p>{weather}</p>
+
+                    {weather.map(x =>
+                        <div>
+                            <p>{x.name}</p>
+                            <p>{x.temp}</p>
+                            <p>{x.humidity}</p>
+                            <p>{x.wind}</p>
+                            <img src={x.iconURL} />
+                        </div>
+                    )}
+
                 </Grid>
             </Grid>
 
