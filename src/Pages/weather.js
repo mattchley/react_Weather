60095ed04d4f46eacee6ec59f1c212c83c1c3383
mattchley <mattchley@gmail.com>
2020-07-17 14:03:36 -0700

    Five Day forecast renders sideways

--- src/Pages/weather.js
+++ src/Pages/weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card'
 import API from '../Utils/API';
 
 
@@ -15,18 +16,15 @@ function Weather() {
         citySearch(search)
     }
 
-    const handleTest = e => {
-        console.log(weather)
-
-    }
-
     const citySearch = search => {
         async function fetchData() {
             let fiveDayArray = await []
             const searchRes = await API.getWeather(search)
             const uvRes = await API.getUV(searchRes.data.coord.lat, searchRes.data.coord.lon)
             const fiveDayRes = await API.getFiveDay(searchRes.data.coord.lat, searchRes.data.coord.lon)
+            // need a systematic comparison of browser time to array time
             for (let index of fiveDayRes.data.list) {
+
                 fiveDayArray.push(
                     {
                         time: index.dt_txt,
@@ -35,7 +33,7 @@ function Weather() {
                 )
             }
 
-            // need a systematic comparison of browser time to array time
+
 
             setWeather([
                 ...weather,
@@ -45,7 +43,7 @@ function Weather() {
                     temp: searchRes.data.main.temp,
                     humidity: searchRes.data.main.humidity,
                     wind: searchRes.data.wind.speed,
-                    icon: `https://openweathermap.org/img/w/${searchRes.data.weather[0].icon}png`,
+                    icon: `https://openweathermap.org/img/w/${searchRes.data.weather[0].icon}.png`,
                     uvIndex: uvRes.data.value,
                     fiveDay: fiveDayArray
                 }
@@ -61,45 +59,49 @@ function Weather() {
     return (
         <Container>
             <h1>What's the weather?</h1>
-            <Grid container spacing={3} justify={"space-evenly"}>
-                <Grid item md={3} xs={12}>
+            <Grid container spacing={3}>
+                <Grid item md={2} xs={12}>
                     <h1>Search here</h1>
                     <input id="citySearch"
                         onChange={(event, value) => setSearch(document.getElementById("citySearch").value)} />
                     <button onClick={handleSubmit}>Search Cities</button>
                 </Grid>
-
-                <Grid item md={6} xs={12}>
+                <Grid item md={10} xs={12}>
                     <h1>Results</h1>
-
-
-                    <button onClick={handleTest}>Test</button>
                     {weather.map(x =>
-                        <div>
+                        <Grid item md={12} xs={12} >
+
                             <p>{x.name}</p>
                             <p>{x.temp}</p>
                             <p>{x.humidity}</p>
                             <p>{x.wind}</p>
                             <p>{x.uvIndex}</p>
                             <img src={x.icon} />
-                            {
-                                x.fiveDay.map(y =>
-                                    <div>
-                                        <p>{y.time}</p>
-                                        <p>{y.temp}</p>
-                                    </div>
-                                )
-                            }
 
-                        </div>
+                        </Grid>
                     )}
-
-
-
                 </Grid>
             </Grid>
+            <Grid container spacing={3}>
+                <Grid item md={2} />
+                <Grid item md={10} xs={12}>
+                    <Grid container spacing={3}>
+                        {weather.map(x =>
+                            x.fiveDay.map(y => (
+                                <Grid item xs={2}>
+                                    <Card>
+                                        <p>{y.time}</p>
+                                        <p>{y.temp}</p>
+                                    </Card>
+                                </Grid>
+                            )
+                            )
 
-        </Container>
+                        )}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container >
     )
 }
 
